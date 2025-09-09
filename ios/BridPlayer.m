a347c59d1589271c661e004e4ef49c4c37a6f25a
Predrag Jevtic <predragjevtic@gmail.com>
2025-09-09 08:14:12 +0200

    Version 1.3.7 romoved [view destroy]

--- ios/BridPlayer.m
+++ ios/BridPlayer.m
@@ -90,6 +90,14 @@ int mediaID;
   [[NSNotificationCenter defaultCenter] removeObserver:self name:@"referenceReactTag" object:@{@"reactTag": [self.reactTag stringValue]}];
 }
 
+- (void)dealloc
+{
+    [_player setPlayerReferenceName:nil];
+    reference = nil;
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"referenceReactTag" object:@{@"reactTag": [self.reactTag stringValue]}];
+}
+
+
 //- (void)removeFromSuperview {
 //  [super removeFromSuperview];
 //  RCTLogInfo(@"[BridPlayer] removeFromSuperview called – destroying player.");
